eac648b33027ed029de82b5dc234c978fc5764a4
M1ingithub <juusonemail13@gmail.com>
2026-02-23 17:25:49 +0200

    23.02.2026-10

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -43,10 +43,10 @@ ALLOWED_HOSTS = [
 ]
 
 # SSL
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-SECURE_SSL_REDIRECT = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SECURE_SSL_REDIRECT = True
 
 #############################################################
 # SRC: https://devcenter.heroku.com/articles/django-assets
