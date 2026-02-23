07ddc4bd0cea0e6678d3d5bd722c59d205cc7561
M1ingithub <juusonemail13@gmail.com>
2026-02-23 16:48:40 +0200

    23.02.2026-5

--- core/settings/prod.py
+++ core/settings/prod.py
@@ -43,9 +43,9 @@ ALLOWED_HOSTS = [
 ]
 
 # SSL
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 # SECURE_SSL_REDIRECT = True
 
 #############################################################
